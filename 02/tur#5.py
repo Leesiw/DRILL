707e800851553dd13a2d33928b3ba9625c17c18e
Leesiw <84753357+Leesiw@users.noreply.github.com>
2021-09-13 15:14:13 +0900

    drill #5 완성 https://youtu.be/chxl5-PpZvc

--- 02/tur#5.py
+++ 02/tur#5.py
@@ -3,23 +3,23 @@ import turtle
 def up():
     turtle.stamp()
     turtle.setheading(90)
-    turtle.forward(100)
+    turtle.forward(50)
 
 def left():
     turtle.stamp()
     turtle.setheading(180)
-    turtle.forward(100)
+    turtle.forward(50)
 
 
 def right():
     turtle.stamp()
     turtle.setheading(0)
-    turtle.forward(100)
+    turtle.forward(50)
 
 def down():
     turtle.stamp()
     turtle.setheading(270)
-    turtle.forward(100)
+    turtle.forward(50)
 
 turtle.shape('turtle')
 turtle.onkey(up, 'w')
