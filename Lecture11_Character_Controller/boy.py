150a3e2fee782eb08fbc3e6bff01b1c2971b65d3
Leesiw <84753357+Leesiw@users.noreply.github.com>
2021-11-04 14:42:43 +0900

    drill #11 완성 https://youtu.be/AcKqWO4ex2M

--- Lecture11_Character_Controller/boy.py
+++ Lecture11_Character_Controller/boy.py
@@ -1,13 +1,17 @@
 from pico2d import *
 
 # Boy Event
-RIGHT_DOWN, LEFT_DOWN, RIGHT_UP, LEFT_UP, SLEEP_TIMER = range(5)
+RIGHT_DOWN, LEFT_DOWN, RIGHT_UP, LEFT_UP, SHIFT_DOWN, SHIFT_UP, SLEEP_TIMER = range(7)
 
 key_event_table = {
     (SDL_KEYDOWN, SDLK_RIGHT): RIGHT_DOWN,
     (SDL_KEYDOWN, SDLK_LEFT): LEFT_DOWN,
     (SDL_KEYUP, SDLK_RIGHT): RIGHT_UP,
-    (SDL_KEYUP, SDLK_LEFT): LEFT_UP
+    (SDL_KEYUP, SDLK_LEFT): LEFT_UP,
+    (SDL_KEYDOWN, SDLK_LSHIFT): SHIFT_DOWN,
+    (SDL_KEYDOWN, SDLK_RSHIFT): SHIFT_DOWN,
+    (SDL_KEYUP, SDLK_LSHIFT): SHIFT_UP,
+    (SDL_KEYUP, SDLK_RSHIFT): SHIFT_UP
 }
 
 
@@ -40,6 +44,8 @@ class IdleState:
             boy.image.clip_draw(boy.frame * 100, 300, 100, 100, boy.x, boy.y)
         else:
             boy.image.clip_draw(boy.frame * 100, 200, 100, 100, boy.x, boy.y)
+        debug_print('State : Idle')
+
 
 class RunState:
     def enter(boy, event):
@@ -67,6 +73,41 @@ class RunState:
            boy.image.clip_draw(boy.frame * 100, 100, 100, 100, boy.x, boy.y)
         else:
             boy.image.clip_draw(boy.frame * 100, 0, 100, 100, boy.x, boy.y)
+        debug_print('State : Run')
+
+
+class DashState:
+    def enter(boy, event):
+        if event == RIGHT_DOWN:
+            boy.velocity += 1
+        elif event == LEFT_DOWN:
+            boy.velocity -= 1
+        elif event == RIGHT_UP:
+            boy.velocity -= 1
+        elif event == LEFT_UP:
+            boy.velocity += 1
+        boy.dir = boy.velocity
+        boy.timer = 500
+
+    def exit(boy, event):
+        pass
+
+    def do(boy):
+        boy.frame = (boy.frame + 1) % 8
+        boy.timer -= 1
+        boy.timer -= 1
+        if boy.timer == 0:
+            boy.add_event(SLEEP_TIMER)
+        boy.x += boy.velocity * 2
+        boy.x = clamp(25, boy.x, 800 - 25)
+
+    def draw(boy):
+        if boy.velocity == 1:
+           boy.image.clip_draw(boy.frame * 100, 100, 100, 100, boy.x, boy.y)
+        else:
+            boy.image.clip_draw(boy.frame * 100, 0, 100, 100, boy.x, boy.y)
+        debug_print('State : Dash')
+
 
 
 class SleepState:
@@ -80,6 +121,7 @@ class SleepState:
     def do(boy):
         boy.frame = (boy.frame + 1) % 8
 
+
     def draw(boy):
         if boy.dir == 1:
             boy.image.clip_composite_draw(boy.frame * 100, 300, 100, 100,
@@ -87,15 +129,24 @@ class SleepState:
         else:
             boy.image.clip_composite_draw(boy.frame * 100, 200, 100, 100,
                                           -3.141592 / 2, '', boy.x + 25, boy.y - 25, 100, 100)
+        debug_print('State : Sleep')
+
 
 next_state_table = {
     IdleState : {RIGHT_UP:RunState, LEFT_UP:RunState,
                  RIGHT_DOWN: RunState, LEFT_DOWN:RunState,
-                 SLEEP_TIMER: SleepState},
-    RunState: {RIGHT_UP:IdleState, LEFT_UP:IdleState,
-                 RIGHT_DOWN: IdleState, LEFT_DOWN:IdleState},
-    SleepState: {LEFT_DOWN:RunState, RIGHT_DOWN:RunState,
-                 LEFT_UP: RunState, RIGHT_UP:RunState},
+                 SLEEP_TIMER: SleepState,
+                 SHIFT_DOWN: IdleState, SHIFT_UP: IdleState},
+    RunState: {RIGHT_UP: IdleState, LEFT_UP:IdleState,
+                 RIGHT_DOWN: IdleState, LEFT_DOWN:IdleState,
+               SHIFT_DOWN: DashState, SHIFT_UP: DashState},
+    SleepState: {LEFT_DOWN: RunState, RIGHT_DOWN: RunState,
+                 LEFT_UP: RunState, RIGHT_UP: RunState,
+                 SHIFT_DOWN: SleepState, SHIFT_UP: SleepState},
+    DashState: {LEFT_DOWN: DashState, RIGHT_DOWN: DashState,
+                 LEFT_UP: IdleState, RIGHT_UP: IdleState,
+                SHIFT_DOWN: DashState, SHIFT_UP: RunState,
+                SLEEP_TIMER: RunState}
 }
 
 
